546e382d9980c15e26e47b59b8b63303831e7355
ysabel06 <45562815+ysabel06@users.noreply.github.com>
2019-02-01 10:25:51 +0100

    Add files via upload

---
+++ contenu.php
@@ -0,0 +1,70 @@
+<div id="contenu" class="container-fluid">
+  <div id="contenu1" class="container-fluid">
+    <div class="page1">
+      <p>vous etes bien sur la page d'acceuil<br>fcjdksfljklfjjdsfdskjfkljfkdxjfkdlsdsjkhjdskhfgjkdshfjdshfkjs</p>
+    </div>
+  </div>
+
+
+<div id="contenu2" class="container-fluid">
+
+          <div class="page2"> 
+            
+             
+                <h3>Expertise<span class="blue">.</span></h3><br>
+                <p>Simplon, c’est une équipe d’experts : la bonne personne au bon endroit
+                 pour une performance optimale.</p>                
+                 <h3>Expérience Utilisateur<span class="blue">.</span></h3><br>
+                 <p>Notre point fort : à l’écoute de l’utilisateur nous travaillons sans relâche à optimiser la solution Inside pour mieux répondre à vos attentes et enjeux. </p>
+                 <h3>Disponibilité et Empathie<span class="blue">.</span></h3><br>
+                 <p>Nous aimons notre métier et la solution que nous développons.
+                   Mais par-dessus-tout, nous aimons être utiles, et faciliter la vie de nos utilisateurs.</p>
+
+                   <img src="https://www.inside-reporting.fr/wp-content/uploads/2016/05/photo-societe-infineo-inside.png">  
+
+
+          </div>
+
+</div>
+
+<div id="contenu3" class="container-fluid">
+          <div class="page3">
+          <h2>L'Equipe de DRH-Inside</h2>
+          <img src="img/storm.png">
+<div class="membres">
+          <h3>Membres :</h3><br>
+          <a href="https://github.com/ysabel06">Ysabelle</a>
+          <br>
+          <a href="https://github.com/Staaz1990">Alexandre</a>
+          <br>
+          <a href="https://github.com/yooo83">Yoann</a>
+          <br>
+          <a href="https://github.com/JeremyViz">Jeremy</a>
+          <br>          </div>
+<div class="simplon">
+          <h3>Groupe Simplon :</h3><br>
+          <a href="https://simplonline.co/">Simplon.com</a>
+
+</div>
+</div>
+</div>
+    <div id="formulaire" class="page4">
+        <p>Remplissez les champs ci-dessous.</p>
+        <div class="row">
+        <form action="/action_page.php">
+          <label for="fname">Prenom :</label>
+            <input type="text" id="fname" name="firstname">
+          <label for="lname">Nom :</label>
+            <input type="text" id="lname" name="lastname">
+          <label for="Email">Adresse e-mail :</label>
+            <input type="text" id="Email" name="E-mail">
+          <label for="Sujet">Sujet :</label>
+            <input type="text" id="Sujet" name="Sujet">
+          <label for="message">Message :</label>
+            <textarea id="message" name="message"></textarea> 
+          <input type="submit" value="Envoyer">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
